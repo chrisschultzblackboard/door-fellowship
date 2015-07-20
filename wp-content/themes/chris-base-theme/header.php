36d785e28307037d720d450122b6666cb8799429
Kitzo <absuser@FrontCounter.ABS.local>
2015-07-20 15:43:06 -0400

    updated

---
+++ wp-content/themes/chris-base-theme/header.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="top" class="hfeed site">
+<header>
+	<div class="col-xs-12">
+	</div>
+</header>
+	<div id="content" class="site-content">
